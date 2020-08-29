f91613f19f75282197507355171163a8cffa4656
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-29 16:40:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
-# Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
